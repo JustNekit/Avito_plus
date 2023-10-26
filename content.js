f4d3002a25f93f3ff3b71ffda68bcb33135b1076
Nikita <nikmukhametshin@gmail.com>
2023-10-26 21:29:58 +0500

    Console output in OOP format

--- content.js
+++ content.js
@@ -1,33 +1,25 @@
-// Функция для извлечения текущей ссылки страницы
 function getCurrentPageUrl() {
   return window.location.href;
 }
 
-// Функция для извлечения значения атрибута элемента
 function extractAttribute(element, attributeName) {
   return element.getAttribute(attributeName);
 }
 
-// Функция для извлечения текстового содержимого элемента
 function extractText(element) {
-  return element.innerText.trim();
+  if (element && element.innerText) {
+    return element.innerText.trim();
+  }
+  return '';
 }
 
-// Функция для удаления request param из URL
-function removeQueryParams(url) {
-  const urlObject = new URL(url);
-  urlObject.search = '';
-  return urlObject.toString();
-}
-
-// Функция для парсинга данных с помощью указанных селекторов
 function parseData() {
   const data = {};
 
   // Селекторы
   const linkSelector = 'a[itemprop="url"]';
   const titleSelector = 'h3[itemprop="name"], h1[itemprop="name"]';
-  const priceSelector = '[itemprop="price"], p[data-marker="item-price"]';
+  const priceSelector = 'p[data-marker="item-price"]';
   const descriptionSelector = 'div[data-marker="item-view/item-description"] p.styles-module-root-_KFFt';
   const dateSelector = 'p[class="styles-module-root-_KFFt styles-module-size_s-awPvv styles-module-size_s_dense-tybDA styles-module-size_s-_P6ZA styles-module-size_dense-z56yO stylesMarginDense-module-root-lyxCd stylesMarginDense-module-paragraph-s-Okkap styles-module-noAccent-nZxz7"], p[class="styles-module-root-_KFFt styles-module-size_s-awPvv styles-module-size_s-_P6ZA stylesMarningNormal-module-root-OSCNq stylesMarningNormal-module-paragraph-s-_c6vD styles-module-noAccent-nZxz7"]';
   const addressSelector = 'span.style-item-address__string-wt61A';
@@ -36,8 +28,9 @@ function parseData() {
   const characteristicsSelector = 'ul.params-paramsList-zLpAu';
   const sendDataSelector = 'a[data-marker="item-send-message-link"]';
   const userLinkSelector = 'a[data-marker="seller-link/link"]';
+  const internslPriceSelector = '[itemprop="price"]'; // Цена в объвлении
 
-  // Извлечение данных
+  // Извлечение элементов
   const linkElements = document.querySelectorAll(linkSelector);
   const titleElements = document.querySelectorAll(titleSelector);
   const priceElements = document.querySelectorAll(priceSelector);
@@ -48,6 +41,7 @@ function parseData() {
   const breadcrumbsElement = document.querySelector(breadcrumbsSelector);
   const characteristicsElement = document.querySelector(characteristicsSelector);
   const userLinkElement = document.querySelector(userLinkSelector);
+  const internalPrice = document.querySelectorAll(internslPriceSelector);
 
   // Применение стилей к выделенным элементам
   linkElements.forEach(element => {
@@ -59,6 +53,10 @@ function parseData() {
   });
 
   priceElements.forEach(element => {
+    element.style.color = 'red';
+  });
+  
+  internalPrice.forEach(element => {
     element.style.color = 'red';
   });
 
@@ -94,50 +92,36 @@ function parseData() {
 
   if (userLinkElement) {
     userLinkElement.style.color = 'red';
-
-    const username = userLinkElement.querySelector('span').innerText;
-    const userLink = userLinkElement.getAttribute('href');
-
-    // Добавляем данные к объекту data
-    data.username = username;
-    data.userLink = userLink;
   }
 
-  const sendDataElement = document.querySelector(sendDataSelector);
-  if (sendDataElement) {
-    sendDataElement.style.color = 'red';
-    data.sendData = extractAttribute(sendDataElement, 'href');
-  }
-
-  // Создаем массив объектов объявлений
+  // Вывод элементов в консоль в формате ООП
   const adsArray = [];
-  
-  // Заполняем массив объявлений объектами, содержащими нужные поля
   for (let i = 0; i < linkElements.length; i++) {
     const adObject = {
-      link: removeQueryParams(extractAttribute(linkElements[i], 'href')), // Удаляем параметры запроса из ссылки
+      link: extractAttribute(linkElements[i], 'href'),
       title: extractText(titleElements[i]),
       price: extractText(priceElements[i]),
+      prices: extractText(internalPrice[i]),
       description: extractText(descriptionElements[i]),
       date: extractText(dateElements[i]),
-      address: extractText(addressElements[i])
+      address: extractText(addressElements[i]),
     };
     adsArray.push(adObject);
   }
 
-  // Создаем новый объект данных в формате ООП
+  // Вывод общей информации в консоль
   const newData = {
     count: adsArray.length,
     array: adsArray
   };
 
-  // Извлечение текущей ссылки страницы
   const currentPageUrl = getCurrentPageUrl();
   newData.currentPageUrl = currentPageUrl;
 
-  // Преобразовываем новый объект данных в JSON и отправляем его
-  chrome.runtime.sendMessage({ action: 'parseAndSendData', data: JSON.stringify(newData) });
+  console.log(JSON.stringify(newData, null, 2)); // Вывод newData в консоль в виде JSON
+
+  // Отправка данных
+  chrome.runtime.sendMessage({ action: 'parseAndSendData', data: newData });
 }
 
-// Вызываем функцию парсинга при загрузке страницы
 parseData();
